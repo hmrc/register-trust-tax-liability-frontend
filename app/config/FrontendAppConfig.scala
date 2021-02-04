bc3bb7138de80aacc3c58d67b77b9baac402f51d
Rajdeep Vijayakar <rajdeep.vijayakar@digital.hmrc.gov.uk>
2021-02-04 12:05:31 +0000

    TRUS-3578: add tracking consent for cookie banner

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -31,7 +31,6 @@ class FrontendAppConfig @Inject() (val configuration: Configuration) {
 
   val repositoryKey: String = "taxLiability"
 
-  val analyticsToken: String = configuration.get[String](s"google-analytics.token")
   val analyticsHost: String = configuration.get[String](s"google-analytics.host")
   val reportAProblemPartialUrl = s"$contactHost/contact/problem_reports_ajax?service=$contactFormServiceIdentifier"
   val reportAProblemNonJSUrl = s"$contactHost/contact/problem_reports_nonjs?service=$contactFormServiceIdentifier"
