34b9658169eb313cdd9f0b0b2c0a2cac95034009
Adam Conder <adamdconder@gmail.com>
2021-03-16 13:01:06 +0000

    TRUS-3901 update play-ui dependency

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,7 +10,7 @@ object AppDependencies {
     "uk.gov.hmrc"       %% "govuk-template"                 % "5.60.0-play-27",
     "uk.gov.hmrc"       %% "play-health"                    % "3.15.0-play-27",
     "uk.gov.hmrc"       %% "play-ui"                        % "9.0.0-play-27",
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.5.0-play-26",
+    "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.5.0-play-27",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-27"     % "2.25.0",
     "uk.gov.hmrc"       %% "tax-year"                       % "1.1.0",
     "uk.gov.hmrc"       %% "play-language"                  % "4.10.0-play-27"
