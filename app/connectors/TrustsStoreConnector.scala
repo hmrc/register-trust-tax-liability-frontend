3b80ead5fe6af5d61c92e961aac7d1681b580d36
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2021-08-13 14:21:41 +0100

    TRUS-4489: Added trusts store connectivity.

---
+++ app/connectors/TrustsStoreConnector.scala
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package connectors
+
+import config.FrontendAppConfig
+import models.Task
+import models.TaskStatus.TaskStatus
+import uk.gov.hmrc.http.HttpReads.Implicits._
+import uk.gov.hmrc.http.{HeaderCarrier, HttpClient, HttpResponse}
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class TrustsStoreConnector @Inject()(http: HttpClient, config: FrontendAppConfig) {
+
+  private val baseUrl: String = s"${config.trustsStoreUrl}/trusts-store"
+
+  def updateTaskStatus(draftId: String, taskStatus: TaskStatus)
+                      (implicit hc: HeaderCarrier, ec: ExecutionContext): Future[HttpResponse] = {
+    val url: String = s"$baseUrl/register/tasks/update-tax-liability/$draftId"
+    http.POST[TaskStatus, HttpResponse](url, taskStatus)
+  }
+
+  def getTaskStatus(draftId: String)
+                   (implicit hc: HeaderCarrier, ec: ExecutionContext): Future[Task] = {
+    val url: String = s"$baseUrl/register/tasks/$draftId"
+    http.GET[Task](url)
+  }
+
+}
+
---
+++ test/services/TrustsStoreServiceSpec.scala
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package services
+
+import base.SpecBase
+import connectors.TrustsStoreConnector
+import models.Task
+import models.TaskStatus.Completed
+import org.mockito.Matchers.{any, eq => eqTo}
+import org.mockito.Mockito.{verify, when}
+import play.api.http.Status.OK
+import uk.gov.hmrc.http.{HeaderCarrier, HttpResponse}
+
+import scala.concurrent.Future
+
+class TrustsStoreServiceSpec extends SpecBase {
+
+  val mockConnector: TrustsStoreConnector = mock[TrustsStoreConnector]
+
+  val featureFlagService = new TrustsStoreService(mockConnector)
+
+  implicit val hc: HeaderCarrier = HeaderCarrier()
+
+  "TrustsStoreService" when {
+
+    ".updateTaskStatus" must {
+      "call trusts store connector" in {
+
+        when(mockConnector.updateTaskStatus(any(), any())(any(), any()))
+          .thenReturn(Future.successful(HttpResponse(OK, "")))
+
+        val result = featureFlagService.updateTaskStatus(draftId, Completed)
+
+        whenReady(result) { res =>
+          res.status mustBe OK
+          verify(mockConnector).updateTaskStatus(eqTo(draftId), eqTo(Completed))(any(), any())
+        }
+      }
+    }
+
+    ".getTaskStatus" must {
+      "call trusts store connector" in {
+
+        when(mockConnector.getTaskStatus(any())(any(), any()))
+          .thenReturn(Future.successful(Task(Completed)))
+
+        val result = featureFlagService.getTaskStatus(draftId)
+
+        whenReady(result) { res =>
+          res mustBe Completed
+          verify(mockConnector).getTaskStatus(eqTo(draftId))(any(), any())
+        }
+      }
+    }
+  }
+}
