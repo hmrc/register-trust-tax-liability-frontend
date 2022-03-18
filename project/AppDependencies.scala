0e0e5e2061c3e97f287ee6d69f42d2c447d6835c
jassalrichy <20478717+jassalrichy@users.noreply.github.com>
2022-03-18 18:13:32 +0000

    [DDCE-2821] - Tighten Content Security Policy

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,11 +6,11 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
     "org.reactivemongo"   %% "play2-reactivemongo"            % "0.20.13-play28",
-    "uk.gov.hmrc"         %% "play-frontend-hmrc"             % "1.1.0-play-28",
-    "uk.gov.hmrc"         %% "play-conditional-form-mapping"  % "1.9.0-play-28",
-    "uk.gov.hmrc"         %% "domain"                         % "6.2.0-play-28",
-    "uk.gov.hmrc"         %% "bootstrap-frontend-play-28"     % "5.9.0",
-    "uk.gov.hmrc"         %% "tax-year"                       % "1.3.0"
+    "uk.gov.hmrc"         %% "play-frontend-hmrc"             % "3.6.0-play-28",
+    "uk.gov.hmrc"         %% "play-conditional-form-mapping"  % "1.11.0-play-28",
+    "uk.gov.hmrc"         %% "domain"                         % "8.0.0-play-28",
+    "uk.gov.hmrc"         %% "bootstrap-frontend-play-28"     % "5.21.0",
+    "uk.gov.hmrc"         %% "tax-year"                       % "1.7.0"
   )
 
   val test: Seq[ModuleID] = Seq(
