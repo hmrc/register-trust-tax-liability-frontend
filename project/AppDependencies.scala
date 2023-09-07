a87533aea995f3c5a8a73cd88a6209359d50b5cc
Stephen <98922374+stephenheath101@users.noreply.github.com>
2023-09-07 10:37:05 +0100

    DDCE-4193 CSP update (#63)
    * DDCE-4193 CSP update
    
    * Update AppDependencies.scala
    
    * Update AppDependencies.scala
    
    * Update AppDependencies.scala

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,6 +6,7 @@ object AppDependencies {
   val bootstrapVersion = "7.21.0"
 
   private val compile: Seq[ModuleID] = Seq(
+    play.sbt.PlayImport.ws,
     "uk.gov.hmrc"         %% "play-frontend-hmrc"             % "7.19.0-play-28",
     "uk.gov.hmrc"         %% "play-conditional-form-mapping"  % "1.13.0-play-28",
     "uk.gov.hmrc"         %% "domain"                         % "8.3.0-play-28",
@@ -15,13 +16,14 @@ object AppDependencies {
 
   private val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                 %% "bootstrap-test-play-28"   % bootstrapVersion,
-    "org.scalatest"               %% "scalatest"                % "3.2.16",
+    "org.scalatest"               %% "scalatest"                % "3.2.17",
     "org.scalatestplus"           %% "scalatestplus-scalacheck" % "3.1.0.0-RC2",
     "org.jsoup"                   %  "jsoup"                    % "1.16.1",
+    "com.typesafe.play"           %% "play-test"                % PlayVersion.current,
     "org.mockito"                 %% "mockito-scala-scalatest"  % "1.17.22",
     "org.scalacheck"              %% "scalacheck"               % "1.17.0",
     "io.github.wolfendale"        %% "scalacheck-gen-regexp"    % "1.1.0",
-    "org.wiremock"                %  "wiremock-standalone"      % "3.0.1",
+    "com.github.tomakehurst"      % "wiremock-standalone"       % "2.27.2",
     "com.vladsch.flexmark"        % "flexmark-all"              % "0.64.8"
   ).map(_ % Test)
 
