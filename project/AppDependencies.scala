5f80416570e50db03465784976fd730aa79acec6
Mark Beresford <mark.beresford@digital.hmrc.gov.uk>
2024-02-29 12:53:59 +0000

    DDCE-5380: Update HMRC logo

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
 
   private val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"         %% "play-frontend-hmrc"             % "7.19.0-play-28",
+    "uk.gov.hmrc"         %% "play-frontend-hmrc-play-28"     % "8.5.0",
     "uk.gov.hmrc"         %% "play-conditional-form-mapping"  % "1.13.0-play-28",
     "uk.gov.hmrc"         %% "domain"                         % "8.3.0-play-28",
     "uk.gov.hmrc"         %% "bootstrap-frontend-play-28"     % bootstrapVersion,
@@ -16,15 +16,15 @@ object AppDependencies {
 
   private val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                 %% "bootstrap-test-play-28"   % bootstrapVersion,
-    "org.scalatest"               %% "scalatest"                % "3.2.17",
+    "org.scalatest"               %% "scalatest"                % "3.2.18",
     "org.scalatestplus"           %% "scalatestplus-scalacheck" % "3.1.0.0-RC2",
     "org.jsoup"                   %  "jsoup"                    % "1.16.1",
     "com.typesafe.play"           %% "play-test"                % PlayVersion.current,
     "org.mockito"                 %% "mockito-scala-scalatest"  % "1.17.22",
     "org.scalacheck"              %% "scalacheck"               % "1.17.0",
     "io.github.wolfendale"        %% "scalacheck-gen-regexp"    % "1.1.0",
-    "com.github.tomakehurst"      % "wiremock-standalone"       % "2.27.2",
-    "com.vladsch.flexmark"        % "flexmark-all"              % "0.64.8"
+    "org.wiremock"                % "wiremock-standalone"       % "3.4.2",
+    "com.vladsch.flexmark"         % "flexmark-all"               % "0.64.8"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
