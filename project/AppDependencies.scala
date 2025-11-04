feea0e07dee067b88b08f6d56d21154382575789
mahesh <175945803+maheshhvr@users.noreply.github.com>
2025-11-04 13:58:01 +0000

    DDCE-6819 Trusts [Frontend] - Implement GOV.UK Rebrand (#73)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,11 +2,11 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootstrapVersion = "9.19.0"
+  val bootstrapVersion = "10.3.0"
 
   private val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"         %% "play-frontend-hmrc-play-30"             % "12.3.0",
+    "uk.gov.hmrc"         %% "play-frontend-hmrc-play-30"             % "12.18.0",
     "uk.gov.hmrc"         %% "play-conditional-form-mapping-play-30"  % "3.3.0",
     "uk.gov.hmrc"         %% "domain-play-30"                         % "11.0.0",
     "uk.gov.hmrc"         %% "bootstrap-frontend-play-30"             % bootstrapVersion,
