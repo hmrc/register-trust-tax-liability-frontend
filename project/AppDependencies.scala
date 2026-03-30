493addac4eac149d63ef88359fa9ee491204581e
alisajadg191 <34246449+alisajadg191@users.noreply.github.com>
2026-03-30 16:52:16 +0100

    DDCE-8267 - Upgrade Bootstrap version and deps

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,12 +2,12 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootstrapVersion = "10.5.0"
+  val bootstrapVersion = "10.7.0"
 
   private val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30"            % "12.29.0",
-    "uk.gov.hmrc" %% "play-conditional-form-mapping-play-30" % "3.4.0",
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30"            % "12.32.0",
+    "uk.gov.hmrc" %% "play-conditional-form-mapping-play-30" % "3.5.0",
     "uk.gov.hmrc" %% "domain-play-30"                        % "11.0.0",
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30"            % bootstrapVersion,
     "uk.gov.hmrc" %% "tax-year"                              % "6.0.0"
@@ -15,7 +15,7 @@ object AppDependencies {
 
   private val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-test-play-30" % bootstrapVersion,
-    "org.scalatestplus" %% "scalacheck-1-18"        % "3.2.19.0"
+    "org.scalatestplus" %% "scalacheck-1-19"        % "3.2.19.0"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
