84ebcc95ee1f1d38a1c30aee8abbaaae5a6349d7
Karishma Charlwood <295779981+karishmacharlwood-hmrc@users.noreply.github.com>
2026-07-13 15:17:53 +0100

    DDCE-8578 [Trusts]: Upgrade play-frontend-hmrc in TrustsFrontends (Repo 6 - 10) (#79)
    * DDCE-8578 register-trust-tax-liability-frontend
    
    * DDCE-8578 Remove stylesheets
    
    * DDCE-8578 Fix errors
    
    * DDCE-8578: adding css & updating sass for the UI fixes
    
    * DDCE-8578: adding css & updating sass for the UI fixes
    
    ---------
    
    Co-authored-by: harish-korada1 <174133905+harish-korada1@users.noreply.github.com>

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,7 +6,7 @@ object AppDependencies {
 
   private val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30"            % "12.32.0",
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30"            % "13.9.0",
     "uk.gov.hmrc" %% "play-conditional-form-mapping-play-30" % "3.5.0",
     "uk.gov.hmrc" %% "domain-play-30"                        % "11.0.0",
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30"            % bootstrapVersion,
@@ -15,7 +15,7 @@ object AppDependencies {
 
   private val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-test-play-30" % bootstrapVersion,
-    "org.scalatestplus" %% "scalacheck-1-19"        % "3.2.19.0"
+    "org.scalatestplus" %% "scalacheck-1-19"        % "3.2.20.0"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
